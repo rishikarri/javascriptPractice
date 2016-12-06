356e707ac0a5846a8a590d484fdf594ec4733b29
Rishi Karri <rishikarri1@gmail.com>
2016-12-05 20:43:10 -0500

    Added in images and styled buttons

--- dragon.js
+++ dragon.js
@@ -9,7 +9,18 @@ function rollDice(){
 	document.images.die1.src = "Images/d" + randomDie1 + ".gif";
 	document.images.die2.src = "Images/d" + randomDie2 + ".gif";
 
-	
-	document.getElementById("userRoll").innerHTML = "You rolled a " + Number(randomDie1 + randomDie2) + "!";
+	var totalRoll = Number(randomDie1 + randomDie2);
+	document.getElementById("userRoll").innerHTML = "You rolled a " + totalRoll + "!";
 
+	// calls function which tells user if they killed or were killed by the dragon
+	// processFight((Number(randomDie1 + randomDie2)))
+}
+
+function displayFightingOptions(){
+	document.getElementById("fighting-instructions").innerHTML = "Wow, you are a brave one indeed. Roll the dice, young one. If you roll a 9 or higher, you will slay the dragon. If not..."
+	document.getElementById("fight-option").className = "visible";
+}
+
+functionprocessFight(rollScore){
+	console.log(rollScore);
 }
